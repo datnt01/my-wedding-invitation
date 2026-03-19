fd8ff68aface280898d8e1e2b82eae38fd49c025
datnt01 <datnt01@gmail.com>
2026-03-19 20:37:24 +0700

    update image

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,21 +1,13 @@
 import coverImage from "./cover.jpg"
 import image1 from "./TAP_1.jpg"
 import image2 from "./TAP_2.jpg"
-import image3 from "./TAP_3.jpg"
 import image4 from "./TAP_4.jpg"
 import image5 from "./TAP_5.jpg"
 import image6 from "./TAP_6.jpg"
-import image7 from "./TAP_7.jpg"
 import image8 from "./TAP_8.jpg"
-import image9 from "./TAP_9.jpg"
-import image10 from "./TAP_10.jpg"
 import image11 from "./TAP_11.jpg"
-import image12 from "./TAP_12.jpg"
 import image13 from "./TAP_13.jpg"
 import image14 from "./TAP_14.jpg"
-import image15 from "./TAP_15.jpg"
-import image16 from "./TAP_16.jpg"
-import image17 from "./TAP_17.jpg"
 import image18 from "./TAP_18.jpg"
 import image19 from "./TAP_19.jpg"
 import image20 from "./TAP_20.jpg"
@@ -26,21 +18,13 @@ export const COVER_IMAGE = coverImage
 export const GALLERY_IMAGES = [
   image1,
   image2,
-  image3,
   image4,
   image5,
   image6,
-  image7,
   image8,
-  image9,
-  image10,
   image11,
-  image12,
   image13,
   image14,
-  image15,
-  image16,
-  image17,
   image18,
   image19,
   image20,
