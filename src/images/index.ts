db373afd346a8a4ebd6af495f21eb82eaf8129d1
datnt01 <datnt01@gmail.com>
2026-03-19 20:32:37 +0700

    update image

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,28 +1,25 @@
-import coverImage from "./cover.jpeg"
-import image1 from "./TAP_1.jpeg"
-import image2 from "./TAP_2.jpeg"
-import image3 from "./TAP_3.jpeg"
-import image4 from "./TAP_4.jpeg"
-import image5 from "./TAP_13.jpeg"
-import image6 from "./TAP_6.jpeg"
-import image7 from "./TAP_7.jpeg"
-import image8 from "./TAP_8.jpeg"
-import image9 from "./TAP_9.jpeg"
-import image10 from "./TAP_10.jpeg"
-import image11 from "./TAP_11.jpeg"
-import image12 from "./TAP_12.jpeg"
-import image13 from "./TAP_13.jpeg"
-import image14 from "./TAP_14.jpeg"
-import image15 from "./TAP_15.jpeg"
-import image17 from "./TAP_17.jpeg"
-import image18 from "./TAP_18.jpeg"
-import image19 from "./TAP_19.jpeg"
-import image20 from "./TAP_20.jpeg"
-import image21 from "./TAP_21.jpeg"
-import image22 from "./TAP_22.jpeg"
-import image23 from "./TAP_23.jpeg"
-import image24 from "./TAP_24.jpeg"
-import image25 from "./TAP_25.jpeg"
+import coverImage from "./cover.jpg"
+import image1 from "./TAP_1.jpg"
+import image2 from "./TAP_2.jpg"
+import image3 from "./TAP_3.jpg"
+import image4 from "./TAP_4.jpg"
+import image5 from "./TAP_5.jpg"
+import image6 from "./TAP_6.jpg"
+import image7 from "./TAP_7.jpg"
+import image8 from "./TAP_8.jpg"
+import image9 from "./TAP_9.jpg"
+import image10 from "./TAP_10.jpg"
+import image11 from "./TAP_11.jpg"
+import image12 from "./TAP_12.jpg"
+import image13 from "./TAP_13.jpg"
+import image14 from "./TAP_14.jpg"
+import image15 from "./TAP_15.jpg"
+import image16 from "./TAP_16.jpg"
+import image17 from "./TAP_17.jpg"
+import image18 from "./TAP_18.jpg"
+import image19 from "./TAP_19.jpg"
+import image20 from "./TAP_20.jpg"
+import image21 from "./TAP_21.jpg"
 
 export const COVER_IMAGE = coverImage
 
@@ -42,13 +39,11 @@ export const GALLERY_IMAGES = [
   image13,
   image14,
   image15,
+  image16,
   image17,
   image18,
   image19,
   image20,
   image21,
-  image22,
-  image23,
-  image24,
-  image25
+
 ]
